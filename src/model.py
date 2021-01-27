2ca54c8ac027668faeb366e17a19df14684eebcc
free-bit <erkin.turkoz@tum.de>
2021-01-27 14:16:25 +0100

    Changes: * Missing 1x1 convolutions added to model after each stage to obtain a single channel pixel-wise score map for edges. * Option for fusing side outputs added to model.

--- src/model.py
+++ src/model.py
@@ -3,29 +3,44 @@ import torch.nn as nn
 from torchvision.models import vgg16, resnet18
 
 class RidgeDetector(nn.Module):
-    def __init__(self):
+    def __init__(self, fuse=False, pretrained=True):
         super().__init__()
-        layers = vgg16(pretrained=True).features
+        layers = vgg16(pretrained=pretrained).features
+        self.fuse = fuse
 
         # Stage-1: Layers 0-3
         self.stage1 = layers[0:4]
-        self.sideout1 = nn.Upsample(scale_factor=1, mode="bilinear", align_corners=True)
-        
+        self.sideout1 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0), # 1x1 conv to shrink channels
+            nn.Upsample(scale_factor=1, mode="bilinear", align_corners=False)
+        )
         # Stage-2: Layers 4-8
         self.stage2 = layers[4:9]
-        self.sideout2 = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
-        
+        self.sideout2 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=1, kernel_size=1, stride=1, padding=0), # 1x1 conv to shrink channels
+            nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
+        )
         # Stage-3: Layers 9-15
         self.stage3 = layers[9:16]
-        self.sideout3 = nn.Upsample(scale_factor=4, mode="bilinear", align_corners=True)
-        
+        self.sideout3 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=1, kernel_size=1, stride=1, padding=0), # 1x1 conv to shrink channels
+            nn.Upsample(scale_factor=4, mode="bilinear", align_corners=False)
+        )
         # Stage-4: Layers 16-22
         self.stage4 = layers[16:23]
-        self.sideout4 = nn.Upsample(scale_factor=8, mode="bilinear", align_corners=True)
-        
+        self.sideout4 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0), # 1x1 conv to shrink channels
+            nn.Upsample(scale_factor=8, mode="bilinear", align_corners=False)
+        )
         # Stage-5: Layers 23-29
         self.stage5 = layers[23:30]
-        self.sideout5 = nn.Upsample(scale_factor=16, mode="bilinear", align_corners=True)
+        self.sideout5 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=1, kernel_size=1, stride=1, padding=0), # 1x1 conv to shrink channels
+            nn.Upsample(scale_factor=16, mode="bilinear", align_corners=False)
+        )
+        # Optional: Fuse side outputs from 5 stages at the end
+        if self.fuse:
+            self.fuse = nn.Conv2d(in_channels=5, out_channels=1, kernel_size=1, stride=1, padding=0)
     
     def forward(self, x):
         # x: NxCxHxW -> y: NxCxHxW -> sideouts: 5xNxCxHxW
@@ -44,8 +59,13 @@ class RidgeDetector(nn.Module):
 
         x = self.stage5(x)
         y5 = self.sideout5(x)
+
+        y = [y1, y2, y3, y4, y5]
+
+        if self.fuse:
+            y = self.fuse(torch.cat(y, dim=1))
         
-        return [y1, y2, y3, y4, y5]
+        return y
 
 def main():
     rd = RidgeDetector()
